0586d7fbb58e4814dff135d0d372ad01f1266b0f
Ruslan V <vruslan@gmail.com>
2025-09-23 18:20:27 +0100

    feat(17-custom-actions): adding packages

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,6 +1,18 @@
 const core = require("@actions/core");
 
 async function run() {
+  /*
+  1. Parse inputs:
+    1.1. base-branch to check for updates
+    1.2. target-branch for PR
+    1.3. GitHub Token for authentication
+    1.4. Working directory
+  2. Execute the npm update command within the working directory
+  3. Check for modified package*.json files
+  4. If modified
+    4.1. Add and commit changes to target-branch
+    4.2. Create PR to the base-branch using the octokit API
+  */
   core.info("I am a custom JS action");
 }
 
