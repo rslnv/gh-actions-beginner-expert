2124b4901dc14c26f57ef363c5488d17a3768068
Ruslan V <vruslan@gmail.com>
2025-09-24 20:11:50 +0100

    feat(17-custom-actions): parsing and validating inputs

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,4 +1,5 @@
 const core = require("@actions/core");
+const exec = require("@actions/exec");
 
 async function run() {
   /*
@@ -13,7 +14,66 @@ async function run() {
     4.1. Add and commit changes to target-branch
     4.2. Create PR to the base-branch using the octokit API
   */
+  const baseBranch = core.getInput("base-branch");
+  const targetBranch = core.getInput("target-branch");
+  const workingDir = core.getInput("working-directory");
+  const ghToken = core.getInput("gh-token");
+  const debug = core.getBooleanInput("debug");
+
+  core.setSecret(ghToken);
+
+  if (!validateBranchName(baseBranch)) {
+    core.setFailed(
+      `Invalid base-branch '${baseBranch}'. Branch names should contain only characters, numbers, hyphens, underscores, dots, and forward slashes`,
+    );
+    return;
+  }
+
+  if (!validateBranchName(targetBranch)) {
+    core.setFailed(
+      `Invalid targer-branch '${targetBranch}'. Branch names should contain only characters, numbers, hyphens, underscores, dots, and forward slashes`,
+    );
+    return;
+  }
+
+  if (!validateDirectoryName(workingDir)) {
+    core.setFailed(
+      `Invalid working-directory '${workingDir}'. Directory names should contain only characters, numbers, hyphens, underscores, and forward slashes`,
+    );
+    return;
+  }
+
+  core.info(`[js-dependency-updae] base-branch = ${baseBranch}`);
+  core.info(`[js-dependency-updae] target-branch = ${targetBranch}`);
+  core.info(`[js-dependency-updae] working-directory = ${workingDir}`);
+
+  await exec.exec("npm update", [], {
+    cwd: workingDir,
+  });
+
+  const gitStatus = await exec.getExecOutput(
+    "git status -s package*.json",
+    [],
+    {
+      cwd: workingDir,
+    },
+  );
+
+  if (gitStatus.stdout.length > 0) {
+    core.info("[js-dependency-updae] updates available");
+  } else {
+    core.info("[js-dependency-updae] no updates");
+  }
+
   core.info("I am a custom JS action");
 }
+
+// letters, digits, _, -, ., /
+const branchNameRegEx = /^[a-zA-Z0-9_\-\.\/]+$/;
+// letters, digits, _, -, /
+const direcoryNameRegEx = /^[a-zA-Z0-9_\-\/]+$/;
+
+const validateBranchName = (name) => branchNameRegEx.test(name);
+const validateDirectoryName = (name) => direcoryNameRegEx.test(name);
 
 run();
